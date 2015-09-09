1ef2ea931b3cb2e2c4bd5e074aa825a78a0d74dc
Stujo <stujo@github.com>
2015-09-09 11:33:52 -0700

    Placeholder Assets

--- app/assets/javascripts/pizzas.js
+++ app/assets/javascripts/pizzas.js
@@ -1,3 +1,7 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+function pizzas_info(){
+  console.log('pizzas rule!');
+}
